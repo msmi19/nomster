0b3f21bae7bdf2856edeac6c37cfa6cf28bb9aa5
Morgan Smith <morgansmith0219@gmail.com>
2019-06-21 18:12:53 +0000

    fixing migration error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost:3030'}
+  config.action_mailer.default_url_options = { host: 'nomster-morgan-smith.heroku.com'}
 
   # Settings specified here will take precedence over those in config/application.rb.
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_210143) do
+ActiveRecord::Schema.define(version: 2019_06_20_220839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
