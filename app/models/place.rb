643cf7ac9f6ca0ac1fa2a0ac44f2dfcaba667044
Morgan Smith <morgansmith0219@gmail.com>
2019-06-23 18:19:16 +0000

    geocoding address added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
   self.per_page = 10
-  validates :name, presence: true
+  validates :name, :address, :description, presence: true
+  validates :name, length: { minimum: 3 }
 end
