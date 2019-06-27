16347b1d9b1d01a90ae4d99c27a3ae2e5bb1ec54
Morgan Smith <morgansmith0219@gmail.com>
2019-06-27 23:35:05 +0000

    update migration 2

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
-class Photo < ApplicationRecord
+class Photo < ApplicationRecord 
+  mound_uploader :picture, PictureUploader
+  
   belongs_to :user
   belongs_to :place
   belongs_to :comment
--- db/migrate/20190625182043_add_picture_to_photos.rb
+++ db/migrate/20190625182043_add_picture_to_photos.rb
@@ -1,5 +1,4 @@
-class Photo < ActiveRecord::Base
-  mound_uploader :picture, PictureUploader
+class AddPictureToPhotos < ActiveRecord::Migration[5.2]
   def change
     add_column :photos, :picture, :string
     add_column :photos, :caption, :string
