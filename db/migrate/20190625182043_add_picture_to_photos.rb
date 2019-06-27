a3f89edb1e813fb06e147b2ad80abb7da0a703fa
Morgan Smith <morgansmith0219@gmail.com>
2019-06-27 23:27:24 +0000

    update migration

--- db/migrate/20190625182043_add_picture_to_photos.rb
+++ db/migrate/20190625182043_add_picture_to_photos.rb
@@ -2,11 +2,7 @@ class Photo < ActiveRecord::Base
   mound_uploader :picture, PictureUploader
   def change
     add_column :photos, :picture, :string
-    t.text :caption
-    t.text :place_id
-  end
-
-    add_index :picture [:user_id, :place_id]
-    add_index :picture, :place_id
+    add_column :photos, :caption, :string
+    add_column :photos, :place_id, :integer
   end 
 end
