cd7255bbada971b46416645d0bb18f63196a1b10
Morgan Smith <morgansmith0219@gmail.com>
2019-06-27 23:39:13 +0000

    update migration3

--- db/migrate/20190625182043_add_picture_to_photos.rb
+++ db/migrate/20190625182043_add_picture_to_photos.rb
@@ -1,7 +1,5 @@
 class AddPictureToPhotos < ActiveRecord::Migration[5.2]
   def change
     add_column :photos, :picture, :string
-    add_column :photos, :caption, :string
-    add_column :photos, :place_id, :integer
   end 
 end
